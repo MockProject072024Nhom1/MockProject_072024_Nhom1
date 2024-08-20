267de2a0461147afdb9d18867d6267b458386c30
Duc Nghia <147839367+ducnghia-sql@users.noreply.github.com>
2024-08-20 15:59:43 +0700

    Update views.py

--- Calendar/views.py
+++ Calendar/views.py
@@ -12,18 +12,18 @@ def bg_calendar(request, bg_id):
     })
 
 def calculateDayOfWeek():
-    week = [0,0,0,0,0,0,0]
+    week = ["0","0","0","0","0","0","0"]
     dt = datetime.now()
     dt_copy = datetime.now()
     week_index = dt.weekday()
     week_index_copy = week_index+1
     for i in range(7):
         if (week_index >= 0):
-            week[week_index] = dt.day
+            week[week_index] = str(dt.day)
             dt = dt - timedelta(days=1)
             week_index -= 1
         else:
             dt_copy = dt_copy + timedelta(days=1)
-            week[week_index_copy] = dt_copy.day
+            week[week_index_copy] = str(dt_copy.day)
             week_index_copy += 1
     return week
