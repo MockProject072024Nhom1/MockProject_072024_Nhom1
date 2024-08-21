54d55a7306156d445a7373144fe88a0db7bca8d5
Nghia03bl <117901265+Nghia03bl@users.noreply.github.com>
2024-08-21 09:46:38 +0700

    Add files via upload

---
+++ Timekeeping/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TimekeepingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'PersonalInfo'
---
+++ Timekeeping/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def bg_personalinfo(request, bg_id):
+    pass
